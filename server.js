4d109f3a9f755fb9eea2913fbb37dab3b639f166
cpeters124 <38111481+cpeters124@users.noreply.github.com>
2018-05-29 22:53:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Jesus is King!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://myblog-my-blog-cp.7e14.starter-us-west-2.openshiftapps.com/`);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Jesus is King!');
+  res.end();
+}).listen(8080);
